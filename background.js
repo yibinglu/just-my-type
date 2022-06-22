06d8e975eac124939b2518994bed60621977e560
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-06-22 14:03:23 -0600

    changed icon

--- background.js
+++ background.js
@@ -1,2 +1,11 @@
-//background.js
-console.log("background.js running!")
+// background.js
+console.log('background.js running!')
+
+chrome.action.onClicked.addListener(iconClicked)
+
+function iconClicked (tab) {
+  const msg = {
+    txt: 'icon clicked'
+  }
+  chrome.tabs.sendMessage(tab.id, msg)
+}
--- content.js
+++ content.js
@@ -1,2 +1,13 @@
-//content.js
-console.log("content.js running!")
+// content.js
+console.log('content.js running!')
+
+// const paragraphs = document.getElementsByTagName('p')
+// for (let i = 0; i < paragraphs.length; i++) {
+//   paragraphs[i].style.color = '#C6C6C6'
+// }
+
+chrome.runtime.onMessage.addListener(gotMessage)
+
+function gotMessage (message, sender, sendResponse) {
+  console.log(message.txt)
+}
