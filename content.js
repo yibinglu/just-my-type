1a04c07cc69ce385a65874fec8fb2fd0fa517413
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-06-24 17:46:34 -0600

    removed p5.js, may add back if needed in the future

--- content.js
+++ content.js
@@ -1,46 +1,55 @@
 // content.js
 /* global chrome */
 
-console.log('content.js running!')
+let toggle = false // fix bug where toggle doesn't reset
+let selectedTextArray = []
 
 chrome.runtime.onMessage.addListener(gotToggleMessage)
-function gotToggleMessage (request, sender, sendResponse) {
-  // receive message from popup that toggle has been switched
-  const toggle = request.message
-  greyText(toggle)
-}
 
 // get mouse click coordinates
-document.onclick = function (e) {
-  const x = e.pageX
-  const y = e.pageY
-  console.log(x, y)
-}
+// document.onclick = e => {
+//   const x = e.pageX
+//   const y = e.pageY
+//   console.log(x, y)
+// }
 
 // detect which element clicked on
-window.onclick = e => {
-  console.log(e.target)
+// window.onclick = e => {
+//   if (e.target.tagName === 'P' && toggle === true) {
+//     selectedTextArray = e.target.textContent.split('')
+//   }
+// }
+
+document.addEventListener('click', getElement)
+
+// function getElement (e) {
+//   if (e.target.tagName === 'P' && toggle === true) {
+//     selectedTextArray = e.target.textContent.split('')
+//     console.log(selectedTextArray)
+//   }
+// }
+
+function getElement (e) {
+  if (e.target.tagName === 'P' && toggle === true) {
+    selectedTextArray = e.target.textContent.split('')
+    console.log(selectedTextArray)
+  }
+}
+
+function gotToggleMessage (request, sender, sendResponse) {
+  // receive message from popup that toggle has been switched
+  toggle = request.message
+  greyText(toggle)
 }
 
 function greyText (toggle) {
   // change text to grey if extension turned on
   const paragraphs = document.getElementsByTagName('p')
   for (let i = 0; i < paragraphs.length; i++) {
-    // const originalColor = paragraphs[i].style.color
     if (toggle) {
       paragraphs[i].style.color = '#B3B3B3'
     } else {
       paragraphs[i].style.color = '#000000'
-    }
-  }
-
-  const lists = document.getElementsByTagName('li')
-  for (let i = 0; i < lists.length; i++) {
-    // const originalColor = lists[i].style.color
-    if (toggle) {
-      lists[i].style.color = '#B3B3B3'
-    } else {
-      lists[i].style.color = '#000000'
     }
   }
 }
