66409a84cebdcf421fa944a6905448dfe4929fb5
Yibing (Amy) Lu <yibingamy@gmail.com>
2022-06-28 16:05:28 -0600

    can now detect if user input is correct or not

--- content.js
+++ content.js
@@ -6,9 +6,6 @@ let selectedTextArray
 
 chrome.runtime.onMessage.addListener(gotToggleMessage)
 
-// document.addEventListener('keydown', function (e) {
-// })
-
 // CALLBACK VER.
 // getText(function (result) {
 //   selectedTextArray = result
@@ -29,28 +26,40 @@ document.addEventListener('click', (e) => {
     getText(e.target.textContent.split(''))
       .then(function (result) {
         selectedTextArray = result
-        console.log(selectedTextArray)
       })
   }
+  console.log(selectedTextArray)
+  processTextArray(selectedTextArray)
 })
 
-function getText (text) {
-  return new Promise((resolve, reject) => {
-    resolve(text)
+async function processTextArray (array) {
+  if (array !== undefined) {
+    for (const char of array) {
+      const keyPressed = await getKeyPressed()
+      console.log('keyPressed: ' + keyPressed)
+      console.log('char: ' + char)
+      if (keyPressed === char) {
+        console.log('correct')
+      } else {
+        console.log('incorrect')
+      }
+    }
+  }
+}
+
+function getKeyPressed () {
+  return new Promise((resolve) => {
+    document.addEventListener('keydown', function (e) {
+      resolve(e.key)
+    })
   })
 }
 
-// ASYNC/AWAIT VER.
-// async function tester (text) {
-//   const result = await getText(text)
-//   return result
-// }
-
-// document.addEventListener('click', (e) => {
-//   if (e.target.tagName === 'P' & toggle === true) {
-//     tester(e.target.textContent.split(''))
-//   }
-// })
+function getText (text) {
+  return new Promise((resolve) => {
+    resolve(text)
+  })
+}
 
 function gotToggleMessage (request, sender, sendResponse) {
   // receive message from popup that toggle has been switched
